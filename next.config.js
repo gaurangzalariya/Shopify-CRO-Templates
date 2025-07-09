48cff3b97aef4d3cfec1c866deb2d880972e2e06
Gaurang Zalariya <zgppatel@gmail.com>
2025-07-09 23:52:01 +0530

    Updating Index and actions

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const repo = 'Shopify-CRO-Templates';
+const nextConfig = {
+  output: 'export',
+  assetPrefix: `/${repo}/`,
+  basePath: `/${repo}`,
+}
+module.exports = nextConfig
