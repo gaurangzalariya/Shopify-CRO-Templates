34f49402be4996539e2e5250ecb37f4c25bbb7a6
Gaurang Zalariya <zgppatel@gmail.com>
2025-07-10 01:04:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'Shopify-CRO-Templates';
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? `/${repo}/` : '',
-  basePath: isProd ? `/${repo}` : '',
 };
 
 module.exports = nextConfig;
